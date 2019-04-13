b49b851703689890d990b8268e646a8190163432
matthewdowney18 <matthew_downey@student.uml.edu>
2019-04-13 14:25:34 -0400

    crated files, and added description

---
+++ ML_model/models.py
@@ -0,0 +1,3 @@
+# this file contains objects for machine learning models
+# models can be initialized with an old model, or randomly
+# each model has a forward function, and a backward for training only i guess

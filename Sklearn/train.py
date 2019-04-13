ae130ad7cf20a8d6a39eab95fd939ad6c4ecc2d6
matthewdowney18 <matthew_downey@student.uml.edu>
2019-04-13 17:03:01 -0400

    train model

--- Sklearn/train.py
+++ Sklearn/train.py
@@ -3,19 +3,32 @@
 import argparse
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import pickle
+import os
 
 def load_data(filename):
     '''
-    takes in the filename of the csv dataset, returns numpy arrays
+    takes in the filename of the csv dataset, returns numpy arrays for the
+    inputs and labels for the train and dev set
 
-    :param filename:
+    probably use pandas to load data, and k-fold cross validation to create
+    def and test sets
+
+    :param filename: (str) csv file with data
     :return:
+        X_train: numpy array (n_train_samples*n_features)
+        y_train: numpy array (n_train_samples)
+        X_dev: numpy array (n_dev_samples*n_features)
+        y_dev: numpy array (n_dev_samples)
     '''
+
+    #sample data
     X_train = np.array([[3, 4], [1, 2]])
     y_train = np.array([2, 4])
     X_val = np.array([[3, 4], [1, 2]])
     y_val = np.array([2, 4])
     return X_train, y_train, X_val, y_val
+
 
 def main():
     parser = argparse.ArgumentParser
@@ -25,14 +38,16 @@ def main():
                         type=str,
                         required=False,
                         help="the input dataset to be used to train the model")
-    parser.add_argument("--model_dir",
-                        default="d",
+    parser.add_argument("--output_dir",
+                        default="test",
                         type=str,
                         required=False,
-                        help="the input dataset to be used to train the model")
-
+                        help="the output file for the ")
 
     args = parser.parse_args()
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
 
     # load data into numpy array
     X_train, y_train, X_val, y_val = load_data(args.data_filename)
@@ -43,7 +58,13 @@ def main():
     # train the model with the x, and y numpy arrays
     model.fit(X_train, y_train)
 
+    score = model.score(X_val, y_val)
+
+    print("score: {}".format(score))
+
     # save the model
+    pickle.dump(model, open(os.path.join(args.output_dir, "trained_model"), 'wb'))
+
     # weights = model.
 
 

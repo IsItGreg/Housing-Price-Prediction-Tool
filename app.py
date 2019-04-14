842fa5817b3d07b01dc1735b1c4f697bdd4652b8
IsItGreg <gsmelkov@gmail.com>
2019-04-14 07:51:50 -0400

    app.py changes

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import time
 
 
 def main():
-    data = pd.read_csv("csv_1.csv")
-    geolocator = Nominatim(user_agent="specify_your_app_name_here")
+    data = pd.read_csv("prop_2019.csv")
+    geolocator = Nominatim(user_agent="TestingThis")
     geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
     newarr = pd.DataFrame({})
@@ -20,22 +20,24 @@ def main():
     newarr["NUM_FULL_BTH"] = data.pop("T_FULL_BTH")
     newarr["NUM_HALF_BTH"] = data.pop("T_HALF_BTH")
 
-    latlong = pd.DataFrame({'LAT': [], 'LON': []})
+    # latlong = pd.DataFrame({'LAT': [], 'LON': []})
 
-    for i, row in data.iterrows():
-        addr = row['ST_NUM'] + ' ' + row['ST_NAME'] + ' ' + row['ST_NAME_SUF'] + ' Boston, MA'
-        location = geolocator.geocode(addr)
-        if location is None:
-            latlong.loc[i] = [42.3602534, -71.0582912]
-        else:
-            latlong.loc[i] = [location.latitude, location.longitude]
-        if i % 100 == 0:
-            print(i)
-        if i % 500 == 0:
-            time.sleep(15)
+    # for i, row in data.iterrows():
+    #     addr = row['ST_NUM'] + ' ' + row['ST_NAME'] + ' ' + row['ST_NAME_SUF'] + ' Boston, MA'
+    #     location = geolocator.geocode(addr)
+    #     if location is None:
+    #         latlong.loc[i] = [42.3602534, -71.0582912]
+    #     else:
+    #         latlong.loc[i] = [location.latitude, location.longitude]
+    #     if i % 100 == 0:
+    #         print(i)
+    #     if i % 500 == 0:
+    #         time.sleep(15)
 
-    newarr["LAT"] = latlong.pop("LAT")
-    newarr["LON"] = latlong.pop("LON")
+    # newarr["LAT"] = latlong.pop("LAT")
+    # newarr["LON"] = latlong.pop("LON")
+
+    newarr["ZIP_CODE"] = data.pop("ZIPCODE")
     newarr["AV_TOTAL"] = data.pop("AV_TOTAL")
 
     newarr.to_csv('formatted.csv', index=False)
